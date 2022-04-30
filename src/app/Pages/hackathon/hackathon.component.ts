b338457dc3b6d42740bab57ada49f674b730276a
Maryem El Ajleni <ajlanimeryam@gmail.com>
2022-04-30 22:25:33 +0200

    timeline fix

--- src/app/Pages/hackathon/hackathon.component.ts
+++ src/app/Pages/hackathon/hackathon.component.ts
@@ -8,8 +8,25 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class HackathonComponent implements OnInit {
 
   constructor() { }
+  currentImage:number=0;
+
+  images=String[5]=[
+    "../../../assets/background/Hackathon/1.jpg",
+    "../../../assets/background/Hackathon/2.jpg",
+    "../../../assets/background/Hackathon/3.jpg",
+    "../../../assets/background/Hackathon/4.jpg",
+    "../../../assets/background/Hackathon/5.jpg"
+  ]
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.changeBackground(); 
+      }, 2000);
   }
-
+  changeBackground():void{
+    if(this.currentImage==4)
+    this.currentImage=0
+    else
+    this.currentImage++
+  }
 }
